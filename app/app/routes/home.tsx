ab3834e08dad0fe3541cc2b4b215e5f872b01352
dollaj <91191491+dollaj@users.noreply.github.com>
2023-05-19 13:13:41 +0000

    aggiunta barra di ricerca

--- app/app/routes/home.tsx
+++ app/app/routes/home.tsx
@@ -6,7 +6,8 @@ import { getOtherUsers } from '~/utils/users.server';
 import { useLoaderData, Outlet } from '@remix-run/react';
 import { getFilteredKudos } from '~/utils/kudos.server'
 import { Kudo } from '~/components/kudo'
-import { Kudo as IKudo, Profile } from '@prisma/client'
+import { Kudo as IKudo, Profile, Prisma } from '@prisma/client'
+import { SearchBar } from '~/components/search-bar'
 
 interface KudoWithProfile extends IKudo {
   author: {
@@ -17,7 +18,41 @@ interface KudoWithProfile extends IKudo {
 export const loader: LoaderFunction = async ({ request }) => {
   const userId = await requireUserId(request)
   const users = await getOtherUsers(userId)
-  const kudos = await getFilteredKudos(userId, {}, {})
+  const url = new URL(request.url)
+  const sort = url.searchParams.get('sort')
+  const filter = url.searchParams.get('filter')
+
+  let sortOptions: Prisma.KudoOrderByWithRelationInput = {}
+  if (sort) {
+    if (sort === 'date') {
+      sortOptions = { createdAt: 'desc' }
+    }
+    if (sort === 'sender') {
+      sortOptions = { author: { profile: { firstName: 'asc' } } }
+    }
+    if (sort === 'emoji') {
+      sortOptions = { style: { emoji: 'asc' } }
+    }
+  }
+
+  // 3
+  let textFilter: Prisma.KudoWhereInput = {}
+  if (filter) {
+    textFilter = {
+      OR: [
+        { message: { mode: 'insensitive', contains: filter } },
+        {
+          author: {
+            OR: [
+              { profile: { is: { firstName: { mode: 'insensitive', contains: filter } } } },
+              { profile: { is: { lastName: { mode: 'insensitive', contains: filter } } } },
+            ],
+          },
+        },
+      ],
+    }
+  }
+  const kudos = await getFilteredKudos(userId, sortOptions, textFilter)
   return json({ users, kudos })
 }
 
@@ -29,7 +64,7 @@ export default function Home() {
       <div className="h-full flex">
         <UserPanel users={users} />
         <div className="flex-1 flex flex-col">
-          {/* Search Bar Goes Here */}
+          <SearchBar />
           <div className="flex-1 flex">
             <div className="w-full p-10 flex flex-col gap-y-4">
               {kudos.map((kudo: KudoWithProfile) => (
