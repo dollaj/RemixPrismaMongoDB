b146ee3627bd8b615484ebf3714a9dff28160a9b
dollaj <91191491+dollaj@users.noreply.github.com>
2023-05-19 12:55:42 +0000

    finita opzione di creazione kudos + preview, aggiunta funzione per l'invio di kudos

---
+++ app/app/utils/kudos.server.ts
@@ -0,0 +1,23 @@
+import { prisma } from './prisma.server'
+import { KudoStyle } from '@prisma/client'
+
+export const createKudo = async (message: string, userId: string, recipientId: string, style: KudoStyle) => {
+  await prisma.kudo.create({
+    data: {
+      // 1
+      message,
+      style,
+      // 2
+      author: {
+        connect: {
+          id: userId,
+        },
+      },
+      recipient: {
+        connect: {
+          id: recipientId,
+        },
+      },
+    },
+  })
+}
