8d22acbab1141bbe5445ac604d762e8083cf916c
dollaj <91191491+dollaj@users.noreply.github.com>
2023-05-19 10:14:22 +0000

    aggiornato schema psima, aggiunta funzione logout e inizio configurazione kudos

--- app/app/components/user-panel.tsx
+++ app/app/components/user-panel.tsx
@@ -1,22 +1,27 @@
 import { prisma } from "~/utils/prisma.server";
+import { User } from '@prisma/client'
+import { UserCircle } from '~/components/user-circle'
 
-
-export function UserPanel() {
+export function UserPanel({ users }: {users: User[]}) {
   return (
     <div className="w-1/6 bg-gray-200 flex flex-col">
       <div className="text-center bg-gray-300 h-20 flex items-center justify-center">
         <h2 className="text-xl text-black-600 font-semibold">My Team</h2>
       </div>
       <div className="flex-1 overflow-y-scroll py-4 flex flex-col gap-y-10">
-        <p>Users go here</p>
+      {users.map(user => (
+       <UserCircle key={user.id} profile={user.profile} className="h-24 w-24 mx-auto flex-shrink-0" />
+      ))}
       </div>
       <div className="text-center p-6 bg-gray-300">
-        <button
-          type="submit"
-          className="bg-zinc-200 rounded-xl font-semibold p-1.5 mt-2 transition duration-300 ease-in-out hover:-translate-y-1"
-        >
-          Sign Out
-        </button>
+        <form action="/logout" method="post">
+          <button
+            type="submit"
+            className="bg-zinc-200 rounded-xl font-semibold p-1.5 mt-2 transition duration-300 ease-in-out hover:-translate-y-1"
+          >
+            Sign Out
+          </button>
+        </form>
       </div>
     </div>
   )
--- app/app/routes/home.tsx
+++ app/app/routes/home.tsx
@@ -2,18 +2,20 @@ import { LoaderFunction, json } from '@remix-run/node'
 import { requireUserId } from '~/utils/auth.server'
 import { UserPanel } from '~/components/user-panel';
 import { Layout } from '~/components/layout';
+import { getOtherUsers } from '~/utils/users.server';
 import { useLoaderData } from '@remix-run/react';
 
 export const loader: LoaderFunction = async ({ request }) => {
-  const userId = await requireUserId(request);
-  return userId;
+  const userId = await requireUserId(request)
+  const users = await getOtherUsers(userId)
+  return json({ users })
 }
 
 export default function Home() {
-  const { userId } = useLoaderData()
+  const { users } = useLoaderData()
   return <Layout>
     <div className='h-full flex'>
-        <UserPanel></UserPanel>
+        <UserPanel users={users}></UserPanel>
     </div>
   </Layout>
 }
